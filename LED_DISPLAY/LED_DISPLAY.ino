2406f0dcd848219fe9a2cbdfff74e9af4b43cff2
Akosiken <54737649+Akosiken@users.noreply.github.com>
2022-02-09 16:24:02 +0800

    Update LED_DISPLAY.ino

--- LED_DISPLAY/LED_DISPLAY.ino
+++ LED_DISPLAY/LED_DISPLAY.ino
@@ -1,117 +1,25 @@
-#define CLK 6
-#define DT 18
-#define SW 19
-#define PIN_LED_RED 8
-#define PIN_LED_GREEN 9
-#define PIN_LED_BLUE 10
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN 10
+#define LED_COUNT 1
+#define DELAY 500
 
-int counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-int mod;
-String currentDir ="";
+Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-  
-  // Set encoder pins as inputs
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
-  pinMode(SW, INPUT_PULLUP);
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_BLUE, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
-  
-  // Setup Serial Monitor
-  Serial.begin(9600);
-
-  // Read the initial state of CLK
-  lastStateCLK = digitalRead(CLK);
+  pixels.begin();
+  pixels.clear();
 }
 
 void loop() {
-  
-  // Read the current state of CLK
-  currentStateCLK = digitalRead(CLK);
-
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
-  if (currentStateCLK != lastStateCLK  && currentStateCLK == 0){
-
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
-    if (digitalRead(DT) != currentStateCLK) {
-      counter++;
-      currentDir ="CW";
-      CW_LED(counter);
-      
-    } 
-    
-    else {
-      counter--;
-      currentDir ="CCW";
-      CCW_LED(counter);
-    }
-    Serial.println(currentDir);
-    Serial.println(counter);
-
-  }
-  lastStateCLK = currentStateCLK;
-  delay(1);
-}
-  if (currentDir == "CW"){
-    
-  }
-
-  // Put in a slight delay to help debounce the reading
-  delay(500);
-  
- }
-
-void rgb_color(int red, int green, int blue){
-  analogWrite(PIN_LED_RED, red);
-  analogWrite(PIN_LED_GREEN, green);
-  analogWrite(PIN_LED_BLUE, blue);
+  LED(255, 0, 0);
+  LED(0, 255, 0);
+  LED(0, 0, 255);
+  LED(255, 255, 255);  
 }
 
-void CCW_LED(int count){
-  mod = count % 4;
-
-  switch(mod){
-    case 1:
-      rgb_color(255, 0, 0);
-      break;
-    case 2:
-      rgb_color(255, 255, 255);
-      break;
-    case 3:
-      rgb_color(0, 0, 255);
-      break;
-    case 0:
-      rgb_color(0, 255, 0);
-      break;
-    default:
-      break;
-  }
-}
-
-void CW_LED(int count){
-  mod = count % 4;
-
-  switch(mod){
-    case 1:
-      rgb_color(255, 0, 0);
-      break;
-    case 2:
-      rgb_color(0, 255, 0);
-      break;
-    case 3:
-      rgb_color(0, 0, 255);
-      break;
-    case 0:
-      rgb_color(255, 255, 255);
-      break;
-    default:
-      break;
-  }
+void LED(int RED, int BLUE, int GREEN){
+  pixels.setPixelColor(0, pixels.Color(RED, BLUE, GREEN));
+  pixels.show();
+  delay(DELAY);
 }
